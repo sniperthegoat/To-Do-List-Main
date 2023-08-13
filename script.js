278e214416254c815bcbbfbefdebb1056fd788c7
Nathan Patel <nathanpatel10@outlook.com>
2023-08-12 21:48:26 -0400

    might be finished idk

--- script.js
+++ script.js
@@ -1,26 +1,81 @@
 const inputBox = document.getElementById("input-box");
+const addButton = document.getElementById("add-button");
 const listContainer = document.getElementById("list-container");
+const undoStack = []; // For storing actions for undo
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
+addButton.addEventListener("click", addTask);
+inputBox.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        addTask();
     }
-    else{
+});
+
+
+document.addEventListener("keydown", function (event) {
+    if ((event.key === "Backspace" || event.key === "Delete") && !inputBox.matches(":focus")) {
+        removeFirstTask();
+    } else if (event.ctrlKey && event.key === "z") {
+        undo();
+    }
+});
+
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        saveData();
+        undoStack.push("add"); // Store action for undo
     }
-    inputBox.value = ""; 
+    inputBox.value = "";
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagname === "LI" ) {
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagname === "SPAN"){
+        saveData();
+        undoStack.push("toggle"); // Store action for undo
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
+        undoStack.push("remove"); // Store action for undo
     }
 }, false);
+
+function removeFirstTask() {
+    const firstTask = listContainer.querySelector("li");
+    if (firstTask) {
+        undoStack.push("remove"); // Store action for undo
+        firstTask.remove();
+        saveData();
+    }
+}
+
+function undo() {
+    const action = undoStack.pop();
+    if (action === "add" || action === "toggle") {
+        const lastTask = listContainer.querySelector("li:last-child");
+        if (lastTask) {
+            lastTask.remove();
+            saveData();
+        }
+    } else if (action === "remove") {
+        showTask(); // Restore the previous state from localStorage
+    }
+}
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
